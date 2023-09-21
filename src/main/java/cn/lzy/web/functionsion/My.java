84724c5a3d57ec18088ca9257eee4d6c71e962c2
xiaoxiaofu <3297408918@qq.com>
2023-09-21 15:52:51 +0800

    9月21日上传Spring MVC 功能拓展实现

---
+++ src/main/java/cn/lzy/web/functionsion/My.java
@@ -0,0 +1,26 @@
+package cn.lzy.web.functionsion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class My implements WebMvcConfigurer, HandlerInterceptor {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/toLoginPage").setViewName("login");
+        registry.addViewController("/love").setViewName("login");
+        registry.addViewController("/login.html").setViewName("login");
+    }
+    @Autowired
+    Mi mi;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(mi)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login.html");
+    }
+}
