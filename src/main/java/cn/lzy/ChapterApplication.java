a8ea797065f774fec44b9ad2342feeab18e1333b
xiaoxiaofu <3297408918@qq.com>
2023-09-25 14:38:28 +0800

    9.25 第五章war架包上传

--- src/main/java/cn/lzy/ChapterApplication.java
+++ src/main/java/cn/lzy/ChapterApplication.java
@@ -2,13 +2,15 @@ package cn.lzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 //启动类
 @SpringBootApplication
 @ImportResource("classpath:xmlproperties.xml")
-public class ChapterApplication {
+/*public class ChapterApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(ChapterApplication.class,args);
@@ -16,17 +18,13 @@ public class ChapterApplication {
 
     }
 
-}
-/*
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
-
-
-@SpringBootApplication
-@ImportResource("classpath:xmlproperties.xml")
-public class ChapterApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ChapterApplication.class, args);
-    }
 }*/
+public class ChapterApplication extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(ChapterApplication.class);
+    }
+    public static void main(String[] args){
+        SpringApplication.run(ChapterApplication.class,args);
+    }
+}
